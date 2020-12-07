19743a053cddbaedd7c0679a92876ccd7ca7a2ba
Bruno Kazuaki Masuda <brunokazuaki@gmail.com>
2020-12-07 20:40:51 -0300

    add comments and convert

--- pass_failt/pass_fail.go
+++ pass_failt/pass_fail.go
@@ -7,23 +7,44 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("Digite a nota: ")
+
+	// Guardar input do usuário
 	reader := bufio.NewReader(os.Stdin)
 	// input, _ := reader.ReadString('\n')
+
+	// Guardar a string digitada até o "enter" (\n)
 	input, err := reader.ReadString('\n')
+
+	// Verificar se há conteúdo no err
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Remover o \n enter do usuário
+	input = strings.TrimSpace(input)
+
+	// Converter a variavel input (antiga string) em valor float 64
+	grade, err := strconv.ParseFloat(input, 64)
+
 	fmt.Println("Your score was: ", input)
 
-	if input >= 60 {
-		status := "Passed"
+	// Criando variavel dentro do escopo da func main, para que seja acessada no final do script
+	var status string
+
+	// If else comparando se a var grade é maior ou igual 60
+	if grade >= 60 {
+		status = "passed"
 	} else {
-		status := "Failed"
+		status = "failed"
 	}
 
+	// Output resultado final
 	fmt.Println("You have: ", status)
 }
