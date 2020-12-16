777349bc6e06814332d9111a08c40c06d2337bbb
Bruno Kazuaki Masuda <brunokazuaki@gmail.com>
2020-12-16 20:44:34 -0300

    guess game done

--- guess_game/guess.go
+++ guess_game/guess.go
@@ -12,31 +12,54 @@ import (
 )
 
 func main() {
+
+	// Criando seed para gerar número aletório
 	seconds := time.Now().Unix()
 	rand.Seed(seconds)
+
+	// Criando um numero aleatorio entre 1 e 100
 	target := rand.Intn(100) + 1
 	fmt.Println("I have chosen a random number between 1 and 100")
 	fmt.Println("Can you guess it?")
-	fmt.Println(target)
+	// Linha para mostrar valor do numero criado fmt.Println(target)
 
+	// COletar input do usuario
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Make a guess: ")
-	input, err := reader.ReadString('\n')
+	success := false
 
-	if err != nil {
-		log.Fatal(err)
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+
+		fmt.Print("Make a guess: ")
+		input, err := reader.ReadString('\n')
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Removendo o \n do usuario
+		input = strings.TrimSpace(input)
+
+		// Convertendo o valor string para int
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
 	}
 
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if guess < target {
-		fmt.Println("Oops. Your guess was LOW")
-	} else if guess > target {
-		fmt.Println("Oops. Your guess was HIGH")
+	// Se a var success for diferente de true
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was:", target)
 	}
 }
