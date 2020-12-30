76182ef40c28fe445b5ef63eb682efe160151f3d
Bruno Kazuaki Masuda <brunokazuaki@gmail.com>
2020-12-30 16:09:00 -0300

    add module

--- pass_failt/pass_fail.go
+++ pass_failt/pass_fail.go
@@ -3,38 +3,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
+	"masuda.com/keyboard"
 )
 
 func main() {
 	fmt.Print("Digite a nota: ")
-
-	// Guardar input do usuário
-	reader := bufio.NewReader(os.Stdin)
-	// input, _ := reader.ReadString('\n')
-
-	// Guardar a string digitada até o "enter" (\n)
-	input, err := reader.ReadString('\n')
-
-	// Verificar se há conteúdo no err
-
+	grade, err := keyboard.GetFloat()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Remover o \n enter do usuário
-	input = strings.TrimSpace(input)
-
-	// Converter a variavel input (antiga string) em valor float 64
-	grade, err := strconv.ParseFloat(input, 64)
-
-	fmt.Println("Your score was: ", input)
-
 	// Criando variavel dentro do escopo da func main, para que seja acessada no final do script
 	var status string
 
